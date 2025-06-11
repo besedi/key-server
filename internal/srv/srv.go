a0c4a65207b5c1a4973be0e57196377fc5c2bdd1
Artem Besedin <temabesedin@nebius.com>
2025-06-11 09:40:36 +0200

    handers_and_metrics

--- internal/srv/srv.go
+++ internal/srv/srv.go
@@ -6,33 +6,56 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/besedi/key-server/internal/metrics"
 )
 
-const maxLen = 100_000
-
-func Serve(size string, port string) {
-	fmt.Println("Starting server..")
-	http.HandleFunc("GET /key/{len}", handlerGenerate)
-	http.HandleFunc("GET /key/", handlerDefault)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+func withRecovery(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recovered from panic: %v", err)
+				http.Error(w, "Internal error", http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
 }
 
-func handlerDefault(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/key/1024", http.StatusFound)
-}
-func handlerGenerate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	len := r.PathValue("len")
-	l, err := strconv.Atoi(len)
+func Serve(size int, port int) {
+	metrics.Init(size)
 
-	if err != nil || l <= 0 || l >= maxLen {
-		http.Error(w, "Invalid length", http.StatusBadRequest)
-		return
+	// handlers
+	http.Handle("GET /key/{len}", metrics.WithMetrics(withRecovery(keyHandler(size))))
+	http.Handle("GET /key/", metrics.WithMetrics(withRecovery(defaultHandler(size))))
+	http.Handle("/metrics", metrics.MetricsHandler())
+
+	srv := "0.0.0.0:" + strconv.Itoa(port)
+	fmt.Println("Max key size is " + strconv.Itoa(size))
+	fmt.Println("Starting server on: " + srv)
+	log.Fatal(http.ListenAndServe(srv, nil))
+}
+
+func defaultHandler(size int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/key/"+strconv.Itoa(size), http.StatusFound)
 	}
+}
 
-	b := make([]byte, l)
-	rand.Read(b)
-	for _, bin := range b {
-		fmt.Fprintf(w, "%08b", bin)
+func keyHandler(size int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		len := r.PathValue("len")
+		l, err := strconv.Atoi(len)
+		if err != nil || l <= 0 || l > size {
+			http.Error(w, "Invalid length. Length should not greater then "+strconv.Itoa(size), http.StatusBadRequest)
+			return
+		}
+		metrics.KeyLengthHistogram.Observe(float64(l))
+		b := make([]byte, l)
+		rand.Read(b)
+		for _, bin := range b {
+			fmt.Fprintf(w, "%08b", bin)
+		}
 	}
 }
